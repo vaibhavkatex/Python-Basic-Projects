411c0e55f4d0cedb6eec366645529d83088ad3f4
vaibhavkatex <Vaibhavkate1301@gmail.com>
2026-06-29 09:17:54 +0530

    Adding a meaningful Comments

--- Q3.py
+++ Q3.py
@@ -1,8 +1,9 @@
-# Q3 - Lists + Functions + Exception Handling
-
+# Create a function manage_marks() that:
+# Stores them in a list.
 def manage_marks():
     marks = []
 
+# Takes 5 subject marks as input from the user.
     try:
         print("Enter marks of 5 subjects:")
 
@@ -12,14 +13,17 @@ def manage_marks():
 
         average = sum(marks) / len(marks)
 
+# Calculates and prints the average, highest, and lowest marks.
         print("\nMarks List:", marks)
         print("Average Marks:", average)
         print("Highest Marks:", max(marks))
         print("Lowest Marks:", min(marks))
 
+# Sorts the list in descending order and prints it.
         marks.sort(reverse=True)
         print("Marks in Descending Order:", marks)
 
+# Handles ValueError if non-numeric input is given.
     except ValueError:
         print("Invalid Input! Please enter only numbers.")
 
