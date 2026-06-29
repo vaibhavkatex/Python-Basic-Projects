cf55038b3126541964a6119f8448caeeae63c7bb
vaibhavkatex <Vaibhavkate1301@gmail.com>
2026-06-29 09:11:13 +0530

    Initial setup

--- Q1.py
+++ Q1.py
@@ -1,1 +1,6 @@
 
+# Q1. Github Account Setup
+# Create a GitHub account (if you donʼt have one).
+# Create a new repository named Python-Basic-Projects.
+# After implementing each feature in each questionʼs solution, commit yourcode with a proper commit message (e.g., "Implement Loop for CountingVowels" or “Create analyze_string function”)
+# After completing the full project, push everything to GitHub.
